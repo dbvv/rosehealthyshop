459c9320f396dac4066a008fb3feb150f2ed3cf7
Daniil Borovkov <daniilborowkow@gmail.com>
2019-11-22 23:17:40 +0600

    Updated TODO

--- inc/single-product.php
+++ inc/single-product.php
@@ -1,3 +1,3 @@
 <?php
 
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
